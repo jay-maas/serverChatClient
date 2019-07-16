f0999b7136525c7935473753c1b349b4f635fc7e
Jay Maas <jaymaas.dev@gmail.com>
2019-07-15 20:34:06 -0600

    Pushing code that enables server to function

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const server = express();
+const http = require('http').createServer(server);
+const io = require('socket.io')(http);
+
+server.use(cors());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.json({success: 'You might just be sane!'});
+});
+
+server.get('/chat', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', socket => {
+  console.log('A user connected.');
+
+  socket.on('sendMessage', msg => {
+    io.emit('sendMessage', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected.');
+  });
+});
+
+const PORT = process.env.PORT || 8000;
+http.listen(PORT, () => console.log(`Server listening on port ${PORT}.`));
+
+module.exports = server;
